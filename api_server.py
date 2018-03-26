fa7bea5708704ddf262fa3e814cb8f206bf58e58
Marcelo Souza <marcelo@marcelosouza.com>
2018-03-26 17:10:18 -0300

    Gunicorn...

--- api_server.py
+++ api_server.py
@@ -1,15 +1,4 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-import json
+from main import application
 
-app = Flask(__name__)
-api = Api(app)
-
-class SplunkNetworkMisuse(Resource):
-    def post(self):
-        return {'result': 'OK'}, 200
-
-api.add_resource(SplunkNetworkMisuse, '/splunk/alert/network/misuse')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    application.run()
---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+
+application = Flask(__name__)
+api = Api(application)
+
+class SplunkNetworkMisuse(Resource):
+    def post(self):
+        return {'result': 'OK'}, 200
+
+api.add_resource(SplunkNetworkMisuse, '/api/splunk/network-misuse')
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
