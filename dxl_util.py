4df688c9eba58da39c67fd73b5cb4332ad8ed4a2
Marcelo Souza <marcelo@marcelosouza.com>
2018-03-27 21:10:05 -0300

    Changes

---
+++ dxl_util.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Marcelo Souza"
+__license__ = "GPL"
+
+# DXL imports
+from dxlclient.client import DxlClient
+from dxlclient.client_config import DxlClientConfig
+from dxlclient.message import Event
+
+#
+# DXL initialization
+#
+def dxl_init(dxl_config):
+    # TODO - enhance error handling here
+    # DxlClientConfig from DXL configuration file
+    logger.info("Loading DXL config from: %s", dxl_config)
+    dxl_config = DxlClientConfig.create_dxl_config_from_file(config['DXL']['Config'])
+
+#
+# Connect to DXL and publish the observables as events
+#
+def dxl_publish(topic, payload):
+    try:
+        with DxlClient(dxl_config) as dxl_client:
+            # Connect to DXL Broker
+            logger.debug("Connecting to DXL broker...")
+            dxl_client.connect()
+            # TODO - handle possible connection errors
+            logger.debug("Publishing message on topic %s."topic)
+            dxl_event = Event(topic)
+            logger.debug("Msg payload: %s", payload)
+            dxl_event.payload = str(payload_str).encode()
+            dxl_client.send_event(dxl_event)
+            logger.debug("Msg published to DXL fabric.")
+    except Exception as e:
+        logger.error("Could not initialize OpenDXL client ({0}).".format(e.message))
+        return False
+    return True
--- server.py
+++ server.py
@@ -7,37 +7,116 @@
 __author__ = "Marcelo Souza"
 __license__ = "GPL"
 
+import sys, logging, argparse, textwrap, os, importlib
+import tokenize
+import dxl_util
 import cherrypy
+#import requests, json, re, urllib3, time
 
-@cherrypy.expose
-class SplunkNetworkMisuse(object):
+# Enable logging, this will also direct built-in DXL and CherryPy log messages.
+# See - https://docs.python.org/2/howto/logging-cookbook.html
+log_formatter = logging.Formatter('%(asctime)s opendxl_webhooks_server (%(name)s) %(levelname)s: %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger = logging.getLogger()
+logger.addHandler(console_handler)
+logging.getLogger('cherrypy').propagate = False
 
-    @cherrypy.tools.accept(media='text/plain')
+# Config
+from configobj import ConfigObj, ConfigObjError
+config = None
 
-    def POST(self, length=8):
-        print("Request Headers: ", cherrypy.request.headers)
-        #body = cherrypy.request.body.read()
-        print("Request Body: ", cherrypy.request.body.read())
-        return "OK"
+def create_arg_parser():
+    """
+    Parses command line arguments.
+    
+    Returns:
+        An ArgumentParser object.
+    """
 
-#if __name__ == "__main__":
-#    try:
-#        main(sys.argv[1:])
-#    except KeyboardInterrupt:
-#        # TODO - gracefully exit
-#        logger.info("Caught keyboard interrupt signal. Exiting...")
-#        exit(0)
+    epilog = """\
+       This script works as ***.
+       """
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=textwrap.dedent(epilog))
+    #parser.add_argument("filter_query", help="Query used to filter desired observables (confidence, type, time window, ...).", metavar="FILTER_QUERY")
+    parser.add_argument("-c", "--configfile", help="Configuration file.", default="/etc/opendxl-webhooks/server.conf")
+    #parser.add_argument("-d", "--dryrun", help="***.", action='store_true', default=False)
+    parser.add_argument("-l", "--loglevel", help="Logging level (DEBUG, INFO or ERROR).", default="INFO")
+    #parser.add_argument("-p", "--pprint", help="Pretty print exported observables to STDOUT.", action='store_true', default=False)
 
-if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                            'server.socket_port': 8001,})
-    conf = {
-        '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'request.method_with_bodies': True,
-            'tools.sessions.on': True,
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        }
-    }
-    cherrypy.quickstart(SplunkNetworkMisuse(), '/webhooks/splunk/network-misuse', conf)
+    return parser
+
+# Plugin
+PluginsMainModule = "__init__"
+def get_plugins(path):
+    """
+    Get plugins.
+    """
+    # get plugins
+    plugins = []
+    possiblePlugins = os.listdir(path)
+    for i in possiblePlugins:
+        location = os.path.join(path, i)
+        full_file_name = (location + "\__init__.py")
+        if not os.path.isdir(location) or not PluginsMainModule + ".py" in os.listdir(location):
+            # TODO - add error msg here
+            continue
+        spec = importlib.util.spec_from_file_location(i, full_file_name)
+        plugins.append({"name": i, "full_file_name": full_file_name, "spec": spec})
+    return plugins
+
+def load_plugin(plugin):
+    """
+    Load plugin.
+    """    
+    spec = plugin["spec"]
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+def main(argv):
+    # parse the args
+    arg_parser = create_arg_parser()
+    args = arg_parser.parse_args()
+
+    # set logging level
+    level = args.loglevel
+    if level == 'DEBUG':
+        logger.setLevel(logging.DEBUG)
+    elif level == 'INFO':
+        logger.setLevel(logging.INFO)
+    elif level == 'WARNING':
+        logger.setLevel(logging.WARNING)   
+    else:
+        logger.setLevel(logging.ERROR)    
+
+    # read cfg file
+    try:
+        config = ConfigObj(args.configfile, raise_errors=True, file_error=True)
+    except:
+        # TODO - enhance error handling here 
+        logger.error("Could not parse config file!")
+        exit(1)
+
+    logger.info("Starting OpenDXL-Webhooks server...")        
+
+    # get plugins and execute their initializers
+    for i in get_plugins(config['Server']['PluginsDir']):
+        logger.debug("Loading plugin %s...", i["name"])
+        plugin = load_plugin(i)
+        # map and call plugin's init() function
+        init = getattr(plugin, 'init')
+        init()
+
+    # setup and run the CherryPy app
+    cherrypy.config.update({'server.socket_host': config['Server']['BindAddress'],
+                            'server.socket_port': int(config['Server']['BindPort']),
+                            'log.screen': config['Server']['CherryPyLoggerEnable'] in ['true', 'True', 'yes', 'Yes'],
+                            })
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
