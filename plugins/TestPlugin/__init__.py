af43aa55e74cb60c6aa57970d1ca0ec5942d5851
Marcelo Souza <marcelo@marcelosouza.com>
2018-03-27 22:14:34 -0300

    Adding TestPlugin

---
+++ plugins/TestPlugin/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Marcelo Souza"
+__license__ = "GPL"
+
+import cherrypy
+
+@cherrypy.expose
+class TestPlugin(object):
+
+    @cherrypy.tools.accept(media='text/plain')
+
+    def POST(self, length=8):
+        print("Request Headers: ", cherrypy.request.headers)
+        #body = cherrypy.request.body.read()
+        print("Request Body: ", cherrypy.request.body.read())
+        return "OK"
+        
+def init():
+  conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'request.method_with_bodies': True,
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        }
+  }
+  cherrypy.tree.mount(TestPluginSplunkNetworkMisuse(), '/webhooks/test', conf)
